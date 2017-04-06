bd3078b70164e9442963de36c4af23250542d55e
Oliver Etchebarne <oliver@icaserver.com>
2017-04-06 12:19:00 -0500

    Fix really badly session ID generator

--- base/libs/Session.php
+++ base/libs/Session.php
@@ -9,9 +9,17 @@ use Vendimia;
 class Session extends MutableCollection {
     function __construct()
     {
+        // Cuando llamamos desde el CLI o del servidor de development, esta 
+        // variable no existe
+        if (!key_exists('HTTP_HOST', $_SERVER)) {
+            $_SERVER['HTTP_HOST'] = 'localhost';
+        }
         // Creamos un session_id en base del proyecto.
         $session_id = md5(Vendimia::$settings['APPID']
-            . $_SERVER['HTTP_HOST']);
+            . $_SERVER['HTTP_HOST']
+            . $_SERVER['REMOTE_ADDR']
+            . time()
+            . (string)mt_rand());
 
         // No iniciamos la sesión si venimos por la CLI
         if (Vendimia::$execution_type != 'cli') {
